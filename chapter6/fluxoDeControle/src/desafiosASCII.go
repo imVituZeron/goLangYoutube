ac948d3c11d466d31e56e76161729727702e9e02
imVituZeron <vitorpaulasantos@gmail.com>
2023-04-26 08:35:27 -0300

    transformando numero em string e mostrando na tabela ascii

---
+++ chapter6/fluxoDeControle/src/desafiosASCII.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	for x := 33; x <= 122; x++ {
+		fmt.Printf("%v\n", string(x)) // convertendo os numeros para o string, e mostrando seus valores.
+	}
+}
