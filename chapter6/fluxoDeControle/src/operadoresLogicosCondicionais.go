83b5a2e2fb9516f8e722243f366bf70b8ddd2ba5
imVituZeron <vitorpaulasantos@gmail.com>
2023-04-29 16:03:14 -0300

    vendo sobre operadores logicos

---
+++ chapter6/fluxoDeControle/src/operadoresLogicosCondicionais.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 4
+	//if x == 2 || x == 3 {
+	//	fmt.Println("x e igual a 2 ou 3") // exemplo de uso do OU
+	//}
+
+	if x > 2 && x == 3 {
+		fmt.Println("x e igual a 2 ou 3") // exemplo de uso do E
+	}
+}
