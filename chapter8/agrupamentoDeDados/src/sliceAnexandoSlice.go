63c65d8cd5b55e14d72dd3dc7e4a3ac383de8c58
imVituZeron <vitorpaulasantos@gmail.com>
2023-05-04 08:32:15 -0300

    detalhes

--- chapter8/agrupamentoDeDados/src/sliceAnexandoSlice.go
+++ chapter8/agrupamentoDeDados/src/sliceAnexandoSlice.go
@@ -10,7 +10,8 @@ func main() {
 
 	outraSlice = append(outraSlice, 9, 10, 11, 12)
 	umaSlice = append(umaSlice, 5, 6, 7, 8)
-	maisUmaSlice := append(umaSlice, outraSlice...)
+	maisUmaSlice := append(umaSlice, outraSlice...) // os tres pontos diz que vai pegar os items dos slice e nao o slice
+	// por default uma slice do tipo int nao aceita um slice.
 
 	fmt.Println(maisUmaSlice)
 }
