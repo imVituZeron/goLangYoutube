928c63ef457a781e6037ce174b848640bdf4463a
imVituZeron <vitorpaulasantos@gmail.com>
2023-05-19 08:11:13 -0300

    capitulo 13 - exec-1

--- lessons/chapter13/src/lesson1.go
+++ lessons/chapter13/src/lesson1.go
@@ -1,1 +1,16 @@
-comecando cap 13
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(retornaInt(1))
+	fmt.Println(retornaIntString(145, "vitor"))
+}
+
+func retornaInt(x int) int {
+	return x + 1
+}
+
+func retornaIntString(x int, nome string) (int, string) {
+	return x, nome
+}
