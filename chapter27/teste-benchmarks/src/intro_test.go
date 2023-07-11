f10048a80168f736651169e67f7685d78096cab7
imVituZeron <vitorpaulasantos@gmail.com>
2023-07-11 07:44:19 -0300

    vendo testes em tabela

--- chapter27/teste-benchmarks/src/intro_test.go
+++ chapter27/teste-benchmarks/src/intro_test.go
@@ -1,5 +1,26 @@
 package main
 
+import testing
+
+type test struct{
+	data []int
+	anwser int
+}
+
+func TestsomaEmTabela(t *testing.T){
+	tests := []test{
+		test{data: []int{1,2,3}, anwser: 6},
+		test{data: []int{10,11,12}, anwser: 33},
+		test{data: []int{5,10,-2}, anwser: 10},
+	}
+	for _, v := range tests {
+		z := soma(v.data...)
+		if z != v.anwser {
+			t.Error("Expected:", v.anwser "Got:", z)
+		}
+	}
+}
+
 func TestSoma(t *testing.T) {
 	teste := soma(3, 2, 1)
 	resultado := 6
